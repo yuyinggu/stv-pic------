514d4dea456f78dd42a5f377ef6f495e93b70af8
Andy Mao <dmao1@mail.sfsu.edu>
2018-08-25 18:03:03 -0700

    添加mp4下载地址 & 分类

--- mp4_downloader.py
+++ mp4_downloader.py
@@ -1,22 +1,23 @@
 import re
+import os
 import requests
 from random import choice
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 ###############    All video source goes here     ###############
-
+mp4_save_path = ''  # mp4文件保存路径
 videos_source = {}
 videos_source['popnews'] = []
 videos_source['pearvideo'] = []
 videos_source['chinanews'] = []
-videos_source['popnews'].append('http://pop.stheadline.com/section.php?cat=new')   # Pop News 最新
-videos_source['popnews'].append('http://pop.stheadline.com/section.php?cat=a')     # Pop News 港闻
-videos_source['popnews'].append('http://pop.stheadline.com/section.php?cat=f')     # Pop News 娱乐
-videos_source['popnews'].append('http://pop.stheadline.com/section.php?cat=b')     # Pop News 国际
-videos_source['popnews'].append('http://pop.stheadline.com/section.php?cat=e')     # Pop News 两岸
-videos_source['popnews'].append('http://pop.stheadline.com/section.php?cat=l')     # Pop News 生活
-videos_source['popnews'].append('http://pop.stheadline.com/section.php?cat=m')     # Pop News 电影
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=new', '最新'])   # Pop News 最新
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=a', '港闻'])     # Pop News 港闻
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=f', '娱乐'])     # Pop News 娱乐
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=b', '国际'])     # Pop News 国际
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=e', '两岸'])     # Pop News 两岸
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=l', '生活'])     # Pop News 生活
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=m', '电影'])     # Pop News 电影
 videos_source['pearvideo'].append('https://www.pearvideo.com/category_2')          # 梨视频 世界
 videos_source['chinanews'].append('https://www.chinanews.com/shipin/')             # 中新网
 
@@ -40,22 +41,27 @@ def random_headers():
     return {'User-Agent': choice(desktop_agents), 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 
 
-# 视频内容下载器
-def video_downloader(file_name, download_link):
+# 视频下载器
+def video_downloader(file_path, file_name, download_link):
     supported_video_format = ['mp4']
     format_supported = False
+    file_path = os.path.join(mp4_save_path, file_path, file_name + '.mp4')
     for video_format in supported_video_format:
         if video_format in download_link:
             format_supported = True
     if not format_supported:
         print('视频格式不支持: %s' % download_link)
         return
-
+    if os.path.exists(file_path):
+        print('{} 已存在'.format(file_path))
+        return
     req = requests.get(download_link)
-    with open(file_name, 'wb') as f:
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    with open(file_path, 'wb') as f:
         f.write(req.content)
         f.close()
-    print("{} downloaded".format(file_name))
+    print("{} downloaded {}".format(file_name, f.name))
 
 
 # 捉取梨视频MP4链接
@@ -71,24 +77,26 @@ def pear_video_handler():
             if re.search('http.*\.mp4', html_decoder(req)):
                 video_link = re.search('http.*\.mp4', html_decoder(req)).group(0)
                 print(video_title, video_sub_page, video_link)
+                video_downloader('pearvideo', video_title, video_link)
 
 
 # 捉取POPNEWS MP4链接
 def pop_news_handler():
     for source in videos_source['popnews']:
-        req = requests.get(source, headers=random_headers())
+        req = requests.get(source[0], headers=random_headers())
         soup = BeautifulSoup(html_decoder(req), 'html.parser')
         videos_list = soup.find('div', id='catPlayListB').find_all('div', class_='trailer')
         for video in videos_list:
             video_sub_page = 'http://pop.stheadline.com/' + video.find('a').get('href')
             video_title = video.find('a').get('title')
             req = requests.get(video_sub_page, headers=random_headers())
-            driver = webdriver.Chrome(executable_path='C:\\Users\\Andy\\Downloads\\chromedriver.exe')
+            driver = webdriver.Chrome(executable_path='chromedriver.exe')
             driver.get(video_sub_page)
             if re.search('http.*\.mp4', driver.page_source):
                 video_link = re.search('http.*\.mp4', driver.page_source).group(0)
                 print(video_title, video_sub_page, video_link)
                 driver.close()
+                video_downloader(os.path.join('popnews', source[1]), video_title.strip(), video_link)
 
 
 # 捉取中新网MP4链接
@@ -107,6 +115,7 @@ def china_news_handler():
             if re.search('http.*\.mp4', html_decoder(req)):
                 video_link = re.search('http.*\.mp4', html_decoder(req)).group(0)
                 print(video_title, video_sub_page, video_link)
+                video_downloader('chinanews', video_title.strip(), video_link)
 
 
 # html 中文内容解码
