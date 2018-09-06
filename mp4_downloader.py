8e4ed4094f771db51a5aacf8925b4449f02e17c6
Andy Mao <dmao1@mail.sfsu.edu>
2018-09-06 01:50:45 -0700

    增加生成 popnews txt文件

--- mp4_downloader.py
+++ mp4_downloader.py
@@ -1,10 +1,22 @@
 #! python
+# -*- coding: utf-8 -*-
 import re
 import os
+import pickle
 import requests
+import argparse
+from ftplib import FTP
 from random import choice
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from datetime import datetime, timedelta
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--user', required=True)
+parser.add_argument('-p', '--password', required=True)
+argv = parser.parse_args()
+
 
 ###############    All video source goes here     ###############
 mp4_save_path = ''  # mp4文件保存路径
@@ -12,13 +24,17 @@ videos_source = {}
 videos_source['popnews'] = []
 videos_source['pearvideo'] = []
 videos_source['chinanews'] = []
-videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=new', '最新'])   # Pop News 最新
+# videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=new', '最新'])   # Pop News 最新
 videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=a', '港闻'])     # Pop News 港闻
 videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=f', '娱乐'])     # Pop News 娱乐
 videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=b', '国际'])     # Pop News 国际
 videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=e', '两岸'])     # Pop News 两岸
 videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=l', '生活'])     # Pop News 生活
 videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=m', '电影'])     # Pop News 电影
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=c', '体育'])     # Pop News 体育
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=d', '财经'])     # Pop News 财经
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=s', '亲子王'])     # Pop News 亲子王
+videos_source['popnews'].append(['http://pop.stheadline.com/section.php?cat=ppl', '名人导航'])     # Pop News 名人导航
 videos_source['pearvideo'].append('https://www.pearvideo.com/category_2')          # 梨视频 世界
 videos_source['chinanews'].append('https://www.chinanews.com/shipin/')             # 中新网
 
@@ -83,6 +99,7 @@ def pear_video_handler():
 
 # 捉取POPNEWS MP4链接
 def pop_news_handler():
+    video_records = {}
     for source in videos_source['popnews']:
         req = requests.get(source[0], headers=random_headers())
         soup = BeautifulSoup(html_decoder(req), 'html.parser')
@@ -90,14 +107,58 @@ def pop_news_handler():
         for video in videos_list:
             video_sub_page = 'http://pop.stheadline.com/' + video.find('a').get('href')
             video_title = video.find('a').get('title')
-            req = requests.get(video_sub_page, headers=random_headers())
+            # req = requests.get(video_sub_page, headers=random_headers())
             driver = webdriver.Chrome(executable_path='chromedriver.exe')
-            driver.get(video_sub_page)
+            driver.set_page_load_timeout(15)
+            try:
+                driver.get(video_sub_page)
+            except:
+                continue
             if re.search('http.*\.mp4', driver.page_source):
                 video_link = re.search('http.*\.mp4', driver.page_source).group(0)
                 print(video_title, video_sub_page, video_link)
                 driver.close()
-                video_downloader(os.path.join('popnews', source[1]), video_title.strip(), video_link)
+                video_records[os.path.basename(video_link)] = [video_title, source[1]]  # 视频链接， 视频标题， 视频分类
+    return video_records
+
+
+def popnews_ftp_comparor():
+    ftp_url = '203.80.0.177'
+    user = argv.user
+    passwd = argv.password
+    today_date = (datetime.now() - timedelta(days=0)).strftime("%Y%m%d")  # 20180904
+    video_records = pop_news_handler()
+
+    # 保存 video_records 测试用途
+    with open('test_records.pkl', 'wb') as f:
+        pickle.dump(video_records, f)
+        f.close()
+
+    ftp = FTP(ftp_url)
+    ftp.login(user, passwd)
+    file_gen = ftp.mlsd('headline/%s' % today_date)
+    ftp.dir('headline/%s' % today_date)
+
+    for fg in file_gen:
+        mp4_name = fg[0]
+        if mp4_name in video_records:
+            video_title = video_records[mp4_name][0]
+            video_cat = video_records[mp4_name][1]
+            csv_name = os.path.join(mp4_save_path, 'popnews%s.csv' % today_date)
+            if os.path.exists(csv_name):
+                with open(csv_name, 'r', encoding='utf-8') as f:
+                    f_content = f.read()
+                    f.close()
+                if mp4_name in f_content:
+                    continue
+        else:
+            video_title = ''
+            video_cat = ''
+
+            with open(csv_name, 'a', encoding='utf-8') as f:
+                f.write('{0},{1},{2}\n'.format(mp4_name, video_title.replace(',', ' '), video_cat))
+                print("找到视频：{0} 标题：{1} 分类：{2}".format(mp4_name, video_title, video_cat))
+                f.close()
 
 
 # 捉取中新网MP4链接
@@ -130,7 +191,7 @@ def html_decoder(req):
 def main():
     for source in videos_source:
         if source == 'popnews':
-            pop_news_handler()
+            popnews_ftp_comparor()
         elif source == 'pearvideo':
             pear_video_handler()
         elif source == 'chinanews':
